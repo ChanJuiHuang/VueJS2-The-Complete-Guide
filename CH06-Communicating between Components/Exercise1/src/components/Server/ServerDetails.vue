f9b3b761b6476c74fc172453a38655beaf7214b1
Chan Jui Huang <ray81415@gmail.com>
2017-05-10 10:36:45 +0800

    What: Add the CH06 and CH06 exercise.
    Why:
    I finished CH06, so I will record it.
    
    How:
    The skills use in CH06 are
    	1. parent component communicate to child component
    	2. child component communicate to parent component
    	3. child component communicate to child component

---
+++ CH06-Communicating between Components/Exercise1/src/components/Server/ServerDetails.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Please select server</p>
+        <div v-if="server">
+            <p>Server Id: {{ server.id }}</p>
+            <p>Server Status: {{ server.status }}</p>
+            <button @click="setNormal">Normal</button>
+            <button @click="setCritical">Critical</button>
+            <button @click="setUnknown">Unknown</button>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '../../main.js'
+    export default {
+        data(){
+            return {
+                server: null
+            }
+        },
+        methods: {
+            setNormal(){
+                this.server.status = 'Normal';
+            },
+            setCritical(){
+                this.server.status = 'Critical';
+            },
+            setUnknown(){
+                this.server.status = 'Unknown';
+            },
+        },
+        created(){
+            eventBus.$on('showDetail', (server) => {
+                this.server = server;
+            });
+        }
+    }
+</script>
+
---
+++ CH06-Communicating between Components/Exercise1/src/components/Server/Servers.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="server in servers"
+                @click="showDetail(server)"
+                style="cursor: pointer">
+                Server #{{ server.id }} #status: {{ server.status }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from '../../main.js';
+    export default {
+        data(){
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Critical'},
+                    {id: 3, status: 'Unknown'},
+                    {id: 4, status: 'Normal'},
+                    {id: 5, status: 'Critical'}
+                ]
+            }
+        },
+        methods: {
+            showDetail(server){
+                eventBus.showDetail(server);
+            }
+        }
+    }
+</script>
